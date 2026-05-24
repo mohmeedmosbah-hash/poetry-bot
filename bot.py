e83ea553d563668e8c119d56f3deb8d49d1245e9
mohmeedmosbah-hash <mohmeedmosbah@gmail.com>
2026-05-24 14:55:40 +0200

    Refactor bot.py to use variables for token and chat ID
    Refactor bot.py to store BOT_TOKEN and CHAT_ID in variables and send a test message via Telegram API.

--- bot.py
+++ bot.py
@@ -1,4 +1,20 @@
+import requests
 import os
 
-print("TOKEN:", os.environ.get("BOT_TOKEN"))
-print("CHAT:", os.environ.get("CHAT_ID"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+print("TOKEN:", BOT_TOKEN)
+print("CHAT:", CHAT_ID)
+
+message = "🎉 اختبار إرسال من GitHub Actions"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+res = requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
+
+print("STATUS:", res.status_code)
+print("RESPONSE:", res.text)
